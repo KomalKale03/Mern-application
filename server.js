a0abca39e49631b5cdd774974aa64102d04982a1
KomalKale03 <125977082+KomalKale03@users.noreply.github.com>
2023-07-05 15:37:03 +0530

    Add files via upload
    Database connection

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const router = express.Router();
+const dotenv = require('dotenv').config();
+const errorHandler = require('./middleware/errorHandler');
+const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017/bank').then(() => {
+    console.log("connection sucessfull")
+}).catch((e) => {
+    console.log("failed to connect");
+})
+
+const port = process.env.PORT || 5000;
+
+
+app.get("/", (req, res) => {
+    res.send("HELLO UR READY FOR AUTHENTICATE")
+})
+
+app.use(cors())
+app.use(express.json());
+app.get('/api', (req, res) => res.send('Server running'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/accounts', require('./routes/accountRoutes'));
+app.use('/api/transactions', require('./routes/transactionRoutes'));
+app.use('/api/admin',require('./routes/adminRoutes'));
+app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','index.html'));
+    })
+}
+
+app.listen(port, () => {
+    console.log(`website is running on ${port}`);
+})
